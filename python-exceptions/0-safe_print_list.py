1d60f387eb40200dd8ec1cb6831f72feb48208c2
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-01-22 17:38:40 +0100

    Add TypeError handling to safe_print_list function

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,9 +2,13 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
-            print(my_list[i], end="")
+            print("{}".format(my_list[i]), end="")
         print()
         return (i + 1)
     except IndexError:
         print()
         return (i)
+    except TypeError:
+        print()
+        return (i)
+    return (i)
