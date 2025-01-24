3941bd1ce78f669229a0a2fb0ecf0390e8e4a4ad
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-01-24 12:53:21 +0100

    Fix documentation for add_integer function parameters

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,13 +5,15 @@ This module provides a function to add two integers.
 Functions:
     add_integer(a, b=98): Returns the integer addition of a and b.
 """
+
+
 def add_integer(a, b=98):
     """
     Adds two integers or floats and returns the result as an integer.
 
     Args:
         a (int, float): The first number to add. Must be an integer or float.
-        b (int, float, optional): The second number to add. Must be an integer or float. Defaults to 98.
+        b (int, float): The second number to add. Must be an integer or float.
 
     Returns:
         int: The sum of a and b, cast to an integer.
