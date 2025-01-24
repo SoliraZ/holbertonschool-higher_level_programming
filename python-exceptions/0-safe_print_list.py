f635d91c2be0894f934f5b6b4cbec55e490ff9cf
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-01-24 15:53:50 +0100

    Refactor safe_print_list to handle IndexError gracefully and simplify return logic

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    i = 0
     try:
         for i in range(x):
             print("{}".format(my_list[i]), end="")
-        print()
-        return (i + 1)
+            i += 1
     except IndexError:
-        print()
-        return (i)
-    except TypeError:
-        print()
-        return (i)
+        pass
+    print()
     return (i)
