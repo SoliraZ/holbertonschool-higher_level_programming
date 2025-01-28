a2c019ea9a419f9b8d1048ea85c6aa8bd3b47079
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-01-28 20:30:29 +0100

    Update 1-rectangle.py

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -13,7 +13,7 @@ class Rectangle:
         if height < 0:
             raise ValueError("height must be >= 0")
         self.__width = width
-        self.height = height
+        self.__height = height
 
     @property
     def width(self):
