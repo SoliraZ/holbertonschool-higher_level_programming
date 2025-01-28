e284774fa9b15f8b87408b2c1c3d9ee8ee04e872
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-01-28 20:39:14 +0100

    Refactor width and height assignment in Rectangle class constructor

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -12,8 +12,8 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         if height < 0:
             raise ValueError("height must be >= 0")
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
