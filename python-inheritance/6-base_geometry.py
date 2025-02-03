c75eceee9ada24bd71a710d576a230dc2fbcbbfa
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-02-03 23:10:31 +0100

    Add BaseGeometry class with unimplemented area method

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class."""
+
+
+class BaseGeometry:
+    """A class with public instance method area."""
+    def area(self):
+        raise Exception("area() is not implemented")
