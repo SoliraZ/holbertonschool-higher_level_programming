327b25a75e251b45a7a876f09f91e64c84a1685a
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-02-04 00:39:01 +0100

    Add Rectangle class that inherits from BaseGeometry with validation

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    def area(self):
+        """Exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """Represent a rectangle."""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
