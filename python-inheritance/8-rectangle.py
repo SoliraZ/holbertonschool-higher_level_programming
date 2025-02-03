1c222a92ba30a6faee39e3532d8575bf560b9b0c
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-02-04 00:49:00 +0100

    Refactor Rectangle class to inherit BaseGeometry and remove redundant code

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,24 +2,12 @@
 """Module for BaseGeometry class."""
 
 
-class BaseGeometry:
-    """Represent base geometry."""
-    def area(self):
-        """Exception"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate value."""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """Represent a rectangle."""
     def __init__(self, width, height):
-        """Initialize a new Rectangle."""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
