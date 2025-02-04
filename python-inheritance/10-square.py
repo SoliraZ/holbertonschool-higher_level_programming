8142a8ae746742738523bb76a12dbc8b31a9a10e
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-02-04 01:09:21 +0100

    Fix initialization order in Square class to ensure size validation occurs before calling superclass constructor

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -8,8 +8,8 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """Represent a square."""
     def __init__(self, size):
-        super().__init__(size, size)
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
