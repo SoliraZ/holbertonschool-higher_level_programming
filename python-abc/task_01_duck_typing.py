69759a132f9014c5bfe4a563b6d0853f4f1033ad
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-02-06 16:23:21 +0100

    Add Shape, Circle, and Rectangle classes with area and perimeter methods

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" Task 01: Duck Typing """
+
+
+from abc import ABC, abstractmethod
+from math import pi
+
+
+class Shape(ABC):
+    """ Class Shape that defines a shape """
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+
+class Circle(Shape):
+    """ Class Circle that defines a circle """
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return ((self.radius * self.radius) * pi)
+
+    def perimeter(self):
+        return (2 * pi * self.radius)
+
+
+class Rectangle(Shape):
+    """ Class Rectangle that defines a rectangle """
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return (self.width * self.height)
+
+    def perimeter(self):
+        return ((self.width + self.height) * 2)
+
+
+def shape_info(info):
+    """ Function to print the area and perimeter of a shape """
+    print("Area: {}".format(info.area()))
+    print("Perimeter: {}".format(info.perimeter()))
