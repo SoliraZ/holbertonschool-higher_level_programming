ff4ff03617500474d9cad6fbe04d7a396d928dfa
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-02-06 12:11:56 +0100

    Add Animal class hierarchy with abstract method for sound implementation

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Task 00: ABC """
+
+
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    @abstractmethod
+    def sound(self):
+        pass
+
+
+class Dog(Animal):
+    def sound(self):
+        return "Bark"
+
+
+class Cat(Animal):
+    def sound(self):
+        return "Meow"
