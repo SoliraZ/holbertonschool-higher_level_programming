0c159f405bc6f59280af473534eb43ba67ba1a9f
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-02-10 15:14:12 +0100

    Refactor class_to_json function to return the object's __dict__ directly

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,9 +2,6 @@
 """ Module that returns the JSON representation of an object (string) """
 
 
-import json
-
-
 def class_to_json(obj):
     """ Function that returns the JSON representation of an object (string) """
-    return (json.dumps(obj.__dict__))
+    return obj.__dict__
