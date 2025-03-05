1325e06fff816982fc2b3be8937d6489dc830810
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-03-05 12:52:16 +0100

    Refactor formatting of database connection parameters in 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -9,11 +9,11 @@ import sys
 if __name__ == "__main__":
     """ Lists all states from the database hbtn_0e_0_usa """
     db = MySQLdb.connect(
-        host = "localhost",
-        port = 3306,
-        user = sys.argv[1],
-        passwd = sys.argv[2],
-        db = sys.argv[3]
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
