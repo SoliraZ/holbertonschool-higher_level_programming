c78441beb4232b074e5d8ad2f04271a63e15b3d1
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-03-06 14:42:15 +0100

    Fix SQL query to order by 'id' instead of 'states.id' for simplicity

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
         db=sys.argv[3]
     )
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     states = cursor.fetchall()
     for state in states:
         print(state)
