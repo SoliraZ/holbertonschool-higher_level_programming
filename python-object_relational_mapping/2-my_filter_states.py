4df4f9a45bab31b1fae2e452409675192961671b
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-03-07 12:49:49 +0100

    Add script to list states from the database based on user input

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Lists all states from the database hbtn_0e_0_usa
+that match the argument. """
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cusor = db.cursor()
+    cusor.execute("SELECT * FROM states WHERE name = '{}' \
+                  ORDER BY id ASC".format(sys.argv[4]))
+    states = cusor.fetchall()
+    for state in states:
+        print(state)
+    cusor.close()
+    db.close()
