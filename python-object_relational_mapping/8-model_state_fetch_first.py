ddf5727f37dd707b6787cd483c5d075012bf67d2
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-03-07 14:05:41 +0100

    Fix typo in query method for fetching the first State object

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -12,12 +12,12 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(username, password, database))
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).orderby(State.id).first()
+    state = session.query(State).order_by(State.id).first()
     if state:
         print("{}: {}".format(state.id, state.name))
     else:
