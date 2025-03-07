2e8c23f487f75c4949def9e08d388cc416f7c0b0
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-03-07 14:14:26 +0100

    Add script to print State object ID by name from the database

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
-""" Lists all state object that contain the letter a
-from the database hbtn_0e_6_usa """
 
+"""Prints the State object with the name passed as argument
+from the database hbtn_0e_6_usa."""
 
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-import sys
-
 
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
+    state_name = sys.argv[4]
+
     engine = create_engine(
         f'mysql+mysqldb://{username}:{password}@localhost:3306/'
         f'{database}'
         )
+    Base.metadata.bind = engine
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).\
-        filter(State.name.like('%a%')).order_by(State.id).all()
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
+    state = session.query(State).filter(State.name == state_name).first()
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
+
     session.close()
